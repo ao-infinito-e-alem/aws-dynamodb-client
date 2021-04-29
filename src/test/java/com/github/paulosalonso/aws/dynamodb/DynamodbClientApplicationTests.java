faec70548b1c108ba65853741b972408c28ebb8f
Paulo Alonso <paulo_alonso_@hotmail.com>
2021-04-29 16:59:11 -0300

    Initial commit

---
+++ src/test/java/com/github/paulosalonso/aws/dynamodb/DynamodbClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.paulosalonso.aws.dynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
